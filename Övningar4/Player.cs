dbb03a10c647a07ab9fef0e62078e0b7231f07ff
Ersvn <112410871+Ersvn@users.noreply.github.com>
2022-10-31 15:32:57 +0100

    Update Player.cs

--- Övningar4/Player.cs
+++ Övningar4/Player.cs
@@ -1,286 +1,191 @@
-using Microsoft.VisualBasic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using SharpDX.Direct3D9;
-using SharpDX.MediaFoundation;
+using SharpDX;
 using System;
 using System.Collections.Generic;
-using System.IO;
-
-
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Color = Microsoft.Xna.Framework.Color;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
+using Microsoft.Xna.Framework.Content;
+using System.Security.Cryptography.X509Certificates;
+using System.Diagnostics.Contracts;
 
 namespace Övningar4
 {
-    public class Game1 : Game
+    public class Player
     {
-        private GraphicsDeviceManager graphics;
-        private SpriteBatch spriteBatch;
-        KeyboardState keyState;
-        private string text;
-        SpriteFont font;
-        List<string> strings = new List<string>();        
-        
-        Player player;
-        static Tile[,] tiles;
-        Princess princess;
-        Texture2D princessTex;
-        //Enemies
-        Enemy enemy;
-        public List<Enemy> enemyList;
-        Texture2D enemyTex;
-        // Texture2D playerTex1;
-        // Texture2D playerTex2;
-        Texture2D runningTex;
-        Texture2D ballTex;
-        Texture2D runTexture;
-        Texture2D wallTileTex;
-        Texture2D floorTileTex;
-        Texture2D bridgeTex;
-        Texture2D bridgeLadderTex;
-        Texture2D ladderTex;
-        Vector2 position;
+        public Vector2 position;
         Vector2 velocity;
-        static int tileSize;
-        //int timer;
-        Texture2D fireballTex;
-        public static bool isVisible;
-        public float speed;
-        int lives;
-        /*
-         * Gör om mappen till gridsystem och bestäm vektor.
-         */
-
-
-        enum GameState { Input, Start, Play, GameOver, } GameState gState;
+        Vector2 destination;
+        Vector2 direction;
+        Vector2 origin;
+        public Texture2D texture, runTexture, fireballTexture;
+        public Texture2D runningTex;
+        public Rectangle boundingBox; 
+        float speed = 450.0f;
+        static bool moving = false;
+        public bool isColliding;
+        public float fireballDelay;
         
+        public float bulletDelay;
+        static bool shooting = false;
 
-        public Game1()
+
+        public Player(Texture2D texture, Texture2D runTexture, Vector2 position)
         {
-            graphics = new GraphicsDeviceManager(this);
-            graphics.PreferredBackBufferWidth = 1200;
-            graphics.PreferredBackBufferHeight = 800;
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
-            graphics.IsFullScreen = false;
-            tileSize = 40;
-            this.Window.Title = "Donkey Kong";
-            lives = 5;
-            isVisible = false;
-
-        }
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
-            base.Initialize();
-        }
-
-        protected override void LoadContent()
-        {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-            font = Content.Load<SpriteFont>(@"Font");
-            wallTileTex = Content.Load<Texture2D>("walltile");
-            floorTileTex = Content.Load<Texture2D>("floortile");
-            ballTex = Content.Load<Texture2D>("SuperMarioFront");
-            runTexture = Content.Load<Texture2D>("SuperMarioBack");
-           // runningTex = Content.Load<Texture2D>("mario-pauline");
-            StreamReader sr = new StreamReader("map.txt");
-            bridgeTex = Content.Load<Texture2D>("bridge");
-            ladderTex = Content.Load <Texture2D>("ladder");
-            enemyTex = Content.Load<Texture2D>("enemy");
-            enemyList = new List<Enemy>();
-            princessTex = Content.Load<Texture2D>("pauline");
-            //enemy = new Enemy(Content.Load < Texture2D > ("enemy"), new Vector2(400, 400), 150);
             
-            text = sr.ReadLine();
+            this.texture = texture;
+            this.runTexture = runTexture;
+            this.position = position;
+            bulletDelay = 20;
+            
+        }
+        
+        public void LoadContent(ContentManager Content)
+        {
+            fireballTexture = Content.Load<Texture2D>("fireball");
+        }
+        public void Update(GameTime gameTime)
+        {
+            
 
-            while (!sr.EndOfStream)
+            if (!moving)
             {
-                strings.Add(sr.ReadLine());
-            }
-            sr.Close();
-
-            tiles = new Tile[strings[0].Length, strings.Count];
-
-            for (int i = 0; i < tiles.GetLength(0); i++)
-            {
-                for (int j = 0; j < tiles.GetLength(1); j++)
+                if (Keyboard.GetState().IsKeyDown(Keys.A))
                 {
-                    if (strings[j][i] == 'w')
-                    {
-                        tiles[i, j] = new Tile(wallTileTex, new Vector2(tileSize * j, tileSize * i), true);
-                    }
-                    if (strings[j][i] == '-')
-                    {
-                        tiles[i, j] = new Tile(floorTileTex, new Vector2(tileSize * j, tileSize * i), true);
-                    }
-                    if (strings[j][i] == '+')
-                    {
-                        tiles[i, j] = new Tile(floorTileTex, new Vector2(tileSize * j, tileSize * i), false);
-                    }
-                    if (strings[j][i] == 'l')
-                    {
-                        tiles[i, j] = new Tile(ladderTex, new Vector2(tileSize * j, tileSize * i), false);
-                    }   
-                    if (strings[j][i] == 'r')
-                    {
-                        tiles[i, j] = new Tile(bridgeTex, new Vector2(tileSize * j, tileSize * i), true);
-                    }
-                    if (strings[j][i] == 'b')
-                    {
-                        tiles[i, j] = new Tile(floorTileTex, new  Vector2(tileSize * j, tileSize * i), false);
-                        player = new Player(ballTex, runTexture, new Vector2(tileSize * i, tileSize * j));
-                    }
-                    if (strings[j][i] == 'e')
-                    {
-                        tiles[i, j] = new Tile(floorTileTex, new Vector2(tileSize * j, tileSize * i), false);
-                        
-                        enemy = new Enemy(enemyTex, new Vector2(tileSize * i, tileSize * j));
-                        enemyList.Add(enemy);
-                    }
-                    if (strings[j][i] == 'p')
-                    {
-                        tiles[i, j] = new Tile(princessTex, new Vector2(tileSize * j, tileSize * i), false);
-                    }
+                    ChangeDirection(new Vector2(-1, 0));
+                }
+                if (Keyboard.GetState().IsKeyDown(Keys.D))
+                {
+                    ChangeDirection(new Vector2(1, 0));
+                }
 
+                if (Keyboard.GetState().IsKeyDown(Keys.S))
+                {
+                    ChangeDirection(new Vector2(0, 1));
+                }
+
+                if (Keyboard.GetState().IsKeyDown(Keys.W))
+                {
+                    ChangeDirection(new Vector2(0, -1));
+                }
+                
+            }
+
+            else
+            {
+                position += direction * speed * (float)gameTime.ElapsedGameTime.TotalSeconds / MathHelper.Pi;
+                //Check if we are near enough to the destination
+                if (Vector2.Distance(position, destination) < 1)
+                {
+                    position = destination;
+                    moving = false;
                 }
             }
-        }
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
+            //if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            //{
+            //    Shoot();
+            //}
 
-            keyState = Keyboard.GetState();
-            switch (gState)
-            {
-                case GameState.Input:
-                    if (keyState.IsKeyDown(Keys.Enter))
-                    {
-                        gState = GameState.Play;
-                    }
-                    break;
+            //UpdateFireballs();  
+            //Fireball//
 
-                case GameState.Play:
-                    //timer--;
-                    {
-                        player.Update(gameTime);
-                        foreach (Enemy enemy in enemyList)
-                        {
-                            enemy.Update(gameTime);
-                            if (player.boundingBox.Contains(enemy.enemyRec.X, enemy.enemyRec.Y))
-                            {
-                                lives--;
-                            }
-                        }
 
-                        
-                    }
 
-                    break;
-                case GameState.GameOver:
-                    {
-                        //.
-                    }
-                    break;
-            }            
+                //if (position.X <= 1) //<---|
+                //    position.X = 1;//<---|
 
-            base.Update(gameTime);
+                //if (position.X >= 900 - texture.Width) //|--->
+                //    position.X = 900 - texture.Width; //|--->
+
+                //if (position.Y <= -12)
+                //    position.Y = -12;
+
+                //if (position.Y >= 660 - texture.Height)
+                //    position.Y = 660 - texture.Height;
+
+
         }
 
-        protected override void Draw(GameTime gameTime)
+        public void Draw(SpriteBatch spriteBatch)
         {
-            GraphicsDevice.Clear(Color.Black);
-
-            spriteBatch.Begin();
-
-            for (int i = 0; i < strings.Count; i++)
+            //if (!moving) spriteBatch.Draw(texture, position, Color.White);
+            //if (moving) spriteBatch.Draw(runTexture, position, Color.White);
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
             {
-                for(int j = 0; j < strings[i].Length; j++)
-                {
-                    if (strings[i][j] == 'w')
-                    {
-                        spriteBatch.Draw(wallTileTex, new Vector2(tileSize * j, tileSize * i), Color.DarkViolet);
-                    }
-
-                    else if (strings[i][j] == '-')
-                    {
-                        spriteBatch.Draw(floorTileTex, new Vector2(tileSize * j, tileSize * i), Color.DarkViolet);
-                    }
-
-                    else if (strings[i][j] == '+')
-                    {
-                        spriteBatch.Draw(floorTileTex, new Vector2(tileSize * j, tileSize * i), Color.DarkViolet);
-                    }
-                    else if (strings[i][j] == 'r')
-                    {
-                        spriteBatch.Draw(bridgeTex, new Vector2(tileSize * j, tileSize * i), Color.BlueViolet);
-                    }
-
-                    else if (strings[i][j] == 'b')
-                    {
-                        spriteBatch.Draw(floorTileTex, new Vector2(tileSize * j, tileSize * i), Color.DarkViolet);
-                    }
-                    else if (strings[i][j] == 'l')
-                    {
-                        spriteBatch.Draw(ladderTex, new Vector2(tileSize * j, tileSize * i), Color.YellowGreen);
-                    }
-                    else if (strings[i][j] == 'e')
-                    {
-                        
-                        spriteBatch.Draw(floorTileTex, new Vector2(tileSize * j, tileSize * i), Color.DarkViolet);
-                        //spriteBatch.Draw(enemyTex, new Vector2(tileSize * j, tileSize * i), Color.Red);
-                        enemy.Draw(spriteBatch);
-                    }
-                    else if (strings[i][j] == 'p')
-                    {
-                        spriteBatch.Draw(floorTileTex, new Vector2(tileSize * j, tileSize * i), Color.DarkViolet);
-                        spriteBatch.Draw(princessTex, new Vector2(tileSize * j, tileSize * i), Color.White);
-                    }
-                } 
+                spriteBatch.Draw(runTexture, position, Color.White);
             }
-            foreach (Enemy enemy in enemyList)
+            //if (Keyboard.GetState().IsKeyDown(Keys.D))
+            //{
+            //    spriteBatch.Draw(runningTex, position, Color.White);
+            //}
+            else
             {
-                enemy.Draw(spriteBatch);
+                spriteBatch.Draw(texture, position, Color.White);
+                
             }
-
-            player.Draw(spriteBatch);
-            spriteBatch.End();
-
-            base.Draw(gameTime);
         }
-        
 
+       
 
-        public static bool GetTileAtPosition(Vector2 vec)
+        public void ChangeDirection(Vector2 dir)
         {
-            return tiles[(int)vec.X / tileSize, (int)vec.Y / tileSize].wall;
+            direction = dir;
+            Vector2 newDestination = position + direction * 40.0f;
+
+            if (!Game1.GetTileAtPosition(newDestination))
+            {
+                destination = newDestination;
+                moving = true;
+            }
         }
-        //public static bool GetLadderAtPosition(Vector2 ladderVec)
+
+        //public void Shoot()
         //{
-        //    return tiles[(int)ladderVec.X / 64, (int)ladderVec.Y / 64].ladder;
-        //}
-        //public static bool GetLadderAtPositionInvisible(Vector2 ladderVec)
-        //{
-        //    return tiles[(int)ladderVec.X / 64, (int)ladderVec.Y / 64].invisible;
-        //}
-        //public void UpdateEnemy()
-        //{
-        //    foreach (Enemy e in enemyList)
+        //    if (bulletDelay >= 0)
+        //        bulletDelay--;
+
+        //    if (bulletDelay <= 0)
         //    {
-        //        e.position.X = e.position.X - e.speed;
+        //        Fireball newFireball = new Fireball(fireballTexture);
+        //        newFireball.position = new Vector2(origin.X - newFireball.texture.Width / 2, origin.Y);
 
-        //        if (e.position.X <= 1200)
-        //            e.isVisible = false;
+        //        newFireball.isVisible = true;
+
+        //        if (fireballList.Count() < 20)
+        //            fireballList.Add(newFireball);
         //    }
-        //    for (int i = 0; i < enemyList.Count; i++)
+        //    if (bulletDelay == 0)
+        //        bulletDelay = 20;
+        //}
+
+        //public void UpdateFireballs()
+        //{
+        //    foreach (Fireball f in fireballList)
         //    {
-        //        if (!enemyList[i].isVisible)
+        //        f.position.X = f.position.X - f.speed;
+
+        //        if (f.position.X <= 1200)
+        //            f.isVisible = false;
+        //    }
+        //    for (int i = 0; i < fireballList.Count; i++)
+        //    {
+        //        if (!fireballList[i].isVisible)
         //        {
-        //            enemyList.RemoveAt(i);
+        //            fireballList.RemoveAt(i);
         //            i--;
         //        }
         //    }
+
+        //}
+        //public void JumpDirection(Vector2 vel)
+        //{
 
         //}
     }
